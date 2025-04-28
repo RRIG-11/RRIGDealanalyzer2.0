746ee4af09dee72befa897d294263e6534d10694
RRIG-11 <209403188+RRIG-11@users.noreply.github.com>
2025-04-28 01:10:21 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// If future backend routes needed, add here
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
